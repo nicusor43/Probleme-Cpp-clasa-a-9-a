d838176ed5e258f92c18f58638c1db88026d8c3c
nicusor43 <53815814+nicusor43@users.noreply.github.com>
2020-05-18 23:25:16 +0300

    update tema5

--- tema5.cpp
+++ tema5.cpp
@@ -8,16 +8,16 @@ int main(){
     int n, k, i, j, l;
     cin >> n >> k;
 
-    for(i = 1; i <= k; i++){
-        for(j = 1; j <= n; j++){
-            for(l = 1; l <= k; l++){
-                t[(i-1) * n +j][l] = i + l - 1;
+    for(i = 1; i <= n; i++){
+        for(j = 1; j <= k; j++){
+            for(l = 1; l <= n; l++){
+                t[(i-1) * k +j][l] = i + l - 1;
             }
         }
     }
 
    for(i = 1; i <= n * k; i++){
-      for(j = 1; j <= k; j++){
+      for(j = 1; j <= n; j++){
          cout << t[i][j] << "  ";
       }
       cout << endl;
